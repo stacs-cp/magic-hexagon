ae1e550714abc4b3dfb9f96f895d6b516027cfda
Özgür Akgün <ozgurakgun@gmail.com>
2023-12-12 23:45:52 +0000

    collect results for multiple timelimits

--- collect-results.py
+++ collect-results.py
@@ -5,88 +5,84 @@ import sys
 import pprint
 
 for model in ["abnormal", "abnormal-implied"]:
+    for timelimit in [10, 60, 5*60, 10*60, None]:
 
-    # timelimit = 10
-    # timelimit = 60
-    # timelimit = 5 * 60
-    # timelimit = 10 * 60
-    timelimit = None
-    countBySolver = {}
-    satisfiableBySolver = {}
+        countBySolver = {}
+        satisfiableBySolver = {}
 
-    allInfo = []
+        allInfo = []
 
-    for dirpath, dirnames, filenames in os.walk(f"conjure-output-{model}"):
-        for infofile in sorted(filenames):
-            if infofile.endswith(".eprime-info"):
-                tmp = infofile.split(".")[0]
-                order = tmp[17:20]
-                shift = tmp[27:31]
-                solver = tmp[33:]
-                # print(tmp, order, shift, solver)
-                info = {}
-                try:
-                    with open(f'{dirpath}/{infofile}', "r") as f:
-                        for l in f:
-                            [k, v] = l.split(":")
-                            info[k.strip()] = v.strip()
-
+        for dirpath, dirnames, filenames in os.walk(f"conjure-output-{model}"):
+            for infofile in sorted(filenames):
+                if infofile.endswith(".eprime-info"):
+                    tmp = infofile.split(".")[0]
+                    order = tmp[17:20]
+                    shift = tmp[27:31]
+                    solver = tmp[33:]
+                    # print(tmp, order, shift, solver)
+                    info = {}
                     try:
-                        info["TotalTime"] = str(
-                            float(info["SolverTotalTime"]) + float(info["SavileRowTotalTime"]))
-                    except:
-                        info["TotalTime"] = "NA"
-                except FileNotFoundError:
-                    pass
-                if info["TotalTime"] != "NA":
-                # if "SolverTotalTime" in info.keys():
-                    allInfo.append(([solver, order, shift], info))
-                    # print(infofile)
-                    if timelimit == None or float(info["TotalTime"]) <= timelimit:
-                        try:
-                            countBySolver[order][solver] += 1
-                        except:
-                            try:
-                                countBySolver[order][solver] = 1
-                            except:
-                                countBySolver[order] = {}
-                                countBySolver[order][solver] = 1
+                        with open(f'{dirpath}/{infofile}', "r") as f:
+                            for l in f:
+                                [k, v] = l.split(":")
+                                info[k.strip()] = v.strip()
 
-                        if info["SolverSatisfiable"] == "1":
+                        try:
+                            info["TotalTime"] = str(
+                                float(info["SolverTotalTime"]) + float(info["SavileRowTotalTime"]))
+                        except:
+                            info["TotalTime"] = "NA"
+                    except FileNotFoundError:
+                        pass
+                    if info["TotalTime"] != "NA":
+                    # if "SolverTotalTime" in info.keys():
+                        allInfo.append(([solver, order, shift], info))
+                        # print(infofile)
+                        if timelimit == None or float(info["TotalTime"]) <= timelimit:
                             try:
-                                satisfiableBySolver[order][solver] += 1
+                                countBySolver[order][solver] += 1
                             except:
                                 try:
-                                    satisfiableBySolver[order][solver] = 1
+                                    countBySolver[order][solver] = 1
                                 except:
-                                    satisfiableBySolver[order] = {}
-                                    satisfiableBySolver[order][solver] = 1
+                                    countBySolver[order] = {}
+                                    countBySolver[order][solver] = 1
 
-    print(model)
-    print("countBySolver")
-    pprint.pprint(countBySolver)
-    for order in ["003", "004", "005", "006", "007", "008"]:
-            print(" ".join([str(countBySolver[order][solver]) for solver in ["chuffed", "kissat", "or-tools"]]))
-    print("satisfiableBySolver")
-    pprint.pprint(satisfiableBySolver)
-    print("\n\n")
+                            if info["SolverSatisfiable"] == "1":
+                                try:
+                                    satisfiableBySolver[order][solver] += 1
+                                except:
+                                    try:
+                                        satisfiableBySolver[order][solver] = 1
+                                    except:
+                                        satisfiableBySolver[order] = {}
+                                        satisfiableBySolver[order][solver] = 1
 
-    maxLength = 0
-    maxNbInv = 0
-    justTheCounts = {}
+        print(model, timelimit)
+        print("countBySolver")
+        pprint.pprint(countBySolver)
+        for order in ["003", "004", "005", "006", "007", "008"]:
+                print(" ".join([str(countBySolver[order][solver]) for solver in ["chuffed", "kissat", "or-tools"]]))
+        print("satisfiableBySolver")
+        pprint.pprint(satisfiableBySolver)
+        print("\n\n")
 
-    headers = set()
-    for _, info in allInfo:
-        headers = headers.union(info.keys())
-    headers = sorted(list(headers))
+        maxLength = 0
+        maxNbInv = 0
+        justTheCounts = {}
 
-    with open(f"results/info-{model}.csv", "w") as out:
-        heading = ", ".join(["solver", "length", "nbInv"] + headers)
-        print(heading, file=out)
-        for [solver, length, nbInv], info in allInfo:
-            maxLength = max(maxLength, int(length))
-            maxNbInv = max(maxNbInv, int(nbInv))
-            if solver == "minionpar":
-                justTheCounts[int(length), int(nbInv)] = info["SolverSolutionsFound"]
-            print(", ".join([solver, length, nbInv] + [info[k] if k in info.keys() else "NA"
-                for k in headers]), file=out)
+        headers = set()
+        for _, info in allInfo:
+            headers = headers.union(info.keys())
+        headers = sorted(list(headers))
+
+        with open(f"results/info-{model}-{timelimit}.csv", "w") as out:
+            heading = ", ".join(["solver", "length", "nbInv"] + headers)
+            print(heading, file=out)
+            for [solver, length, nbInv], info in allInfo:
+                maxLength = max(maxLength, int(length))
+                maxNbInv = max(maxNbInv, int(nbInv))
+                if solver == "minionpar":
+                    justTheCounts[int(length), int(nbInv)] = info["SolverSolutionsFound"]
+                print(", ".join([solver, length, nbInv] + [info[k] if k in info.keys() else "NA"
+                    for k in headers]), file=out)
